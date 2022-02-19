aafc26afafeff5d32a1d657fc5ca4a774e151805
Jay <jayzawrotny@gmail.com>
2022-02-19 00:37:20 -0500

    Switched to legit tailwind.css builds

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	content: ["./docs/**/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				"notion-dark": "#2F3437",
+			},
+			screens: {
+				"device-sm": { raw: "(min-device-width: 640px)" },
+			},
+			boxShadow: {
+				inner: "0 1px 1px 0 rgba(255, 255, 255, 0.3) inset",
+			},
+		},
+	},
+}
